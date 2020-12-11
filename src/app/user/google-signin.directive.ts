53979ea547b69bfee34296fd43ea3ca258268388
Sathya Narayanan S <sathyassn@gmail.com>
2020-12-10 22:58:39 -0500

    Angular Kanban app - Google SignIn directive created, login-page comp & app mod  updated)

---
+++ src/app/user/google-signin.directive.ts
@@ -0,0 +1,15 @@
+import { Directive, HostListener } from '@angular/core';
+import { AngularFireAuth } from '@angular/fire/auth';
+import firebase from 'firebase';
+
+@Directive({
+  selector: '[appGoogleSignin]',
+})
+export class GoogleSigninDirective {
+  constructor(private afAuth: AngularFireAuth) {}
+
+  @HostListener('click')
+  onClick() {
+    this.afAuth.signInWithPopup(new firebase.auth.GoogleAuthProvider());
+  }
+}
--- src/app/user/login-page/login-page.component.ts
+++ src/app/user/login-page/login-page.component.ts
@@ -1,12 +1,15 @@
-import { Component, OnInit } from '@angular/core';
+import { Component } from '@angular/core';
+import { AngularFireAuth } from '@angular/fire/auth';
 
 @Component({
   selector: 'app-login-page',
   templateUrl: './login-page.component.html',
   styleUrls: ['./login-page.component.scss'],
 })
-export class LoginPageComponent implements OnInit {
-  constructor() {}
+export class LoginPageComponent {
+  constructor(public afAuth: AngularFireAuth) {}
 
-  ngOnInit(): void {}
+  signOut() {
+    this.afAuth.signOut();
+  }
 }
