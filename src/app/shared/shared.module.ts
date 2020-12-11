238a671879d90f5af4a94afac0d1635c26b683da
Sathya Narayanan S <sathyassn@gmail.com>
2020-12-10 20:53:58 -0500

    Angular Kanban app - Shared Module added with necessary modules for the project

---
+++ src/app/shared/shared.module.ts
@@ -0,0 +1,43 @@
+// Angular Modules
+import { NgModule } from '@angular/core';
+import { CommonModule } from '@angular/common';
+import { RouterModule } from '@angular/router';
+// import { ReactiveFormsModule } from '@angular/forms';
+
+// Angular Material/CDK modules
+import { MatButtonModule } from '@angular/material/button';
+import { MatToolbarModule } from '@angular/material/toolbar';
+import { MatIconModule } from '@angular/material/icon';
+import { LayoutModule } from '@angular/cdk/layout';
+import { MatSidenavModule } from '@angular/material/sidenav';
+import { MatListModule } from '@angular/material/list';
+import { MatMenuModule } from '@angular/material/menu';
+import { MatCardModule } from '@angular/material/card';
+import { MatFormFieldModule } from '@angular/material/form-field';
+import { MatInputModule } from '@angular/material/input';
+import { MatSnackBarModule } from '@angular/material/snack-bar';
+
+// Internal Modules
+const components = [];
+const modules = [
+  CommonModule,
+  RouterModule,
+  MatButtonModule,
+  MatToolbarModule,
+  MatIconModule,
+  LayoutModule,
+  MatSidenavModule,
+  MatListModule,
+  MatMenuModule,
+  MatCardModule,
+  MatFormFieldModule,
+  MatInputModule,
+  MatSnackBarModule,
+];
+
+@NgModule({
+  declarations: [...components],
+  imports: [...modules],
+  exports: [...modules, ...components],
+})
+export class SharedModule {}
