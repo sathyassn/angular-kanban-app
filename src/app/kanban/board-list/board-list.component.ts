adb6ef8f459ddba4a622682caaa0d4bc6dabde8b
Sathya Narayanan S <sathyassn@gmail.com>
2020-12-11 10:03:28 -0500

    Angular Kanban app - kanban board-list comp updated

--- src/app/kanban/board-list/board-list.component.ts
+++ src/app/kanban/board-list/board-list.component.ts
@@ -1,15 +1,34 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnDestroy, OnInit } from '@angular/core';
+import { CdkDragDrop, moveItemInArray } from '@angular/cdk/drag-drop';
+import { Subscription } from 'rxjs';
+import { MatDialog, MatDialogConfig } from '@angular/material/dialog';
+
+import { Board } from '../board.model';
+import { BoardService } from '../board.service';
 
 @Component({
   selector: 'app-board-list',
   templateUrl: './board-list.component.html',
-  styleUrls: ['./board-list.component.scss']
+  styleUrls: ['./board-list.component.scss'],
 })
-export class BoardListComponent implements OnInit {
+export class BoardListComponent implements OnInit, OnDestroy {
+  boards: Board[] = [];
+  sub: Subscription = new Subscription();
 
-  constructor() { }
+  constructor(private boardService: BoardService, private dialog: MatDialog) {}
 
   ngOnInit(): void {
+    this.sub = this.boardService.getUserBoards().subscribe((boards) => {
+      this.boards = boards;
+    });
   }
 
+  ngOnDestroy() {
+    this.sub.unsubscribe();
+  }
+
+  drop(event: CdkDragDrop<string[]>) {
+    moveItemInArray(this.boards, event.previousIndex, event.currentIndex);
+    this.boardService.sortBoards(this.boards);
+  }
 }
