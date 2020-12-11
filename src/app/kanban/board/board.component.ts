2792455a9d1c4b4e51e2a9615b8298cb4f950b8f
Sathya Narayanan S <sathyassn@gmail.com>
2020-12-11 10:39:48 -0500

    Angular Kanban app - board comp updated to open task-dialog comp

--- src/app/kanban/board/board.component.ts
+++ src/app/kanban/board/board.component.ts
@@ -1,9 +1,10 @@
 import { CdkDragDrop, moveItemInArray } from '@angular/cdk/drag-drop';
 import { Component, Input } from '@angular/core';
-import { MatDialog } from '@angular/material/dialog';
+import { MatDialog, MatDialogConfig } from '@angular/material/dialog';
 
-import { Board } from '../board.model';
+import { Board, Task } from '../board.model';
 import { BoardService } from '../board.service';
+import { TaskDialogComponent } from '../dialogs/task-dialog/task-dialog.component';
 
 @Component({
   selector: 'app-board',
@@ -24,5 +25,41 @@ export class BoardComponent {
       );
       this.boardService.updateTasks(this.board.id, this.board.tasks);
     }
+  }
+
+  openTaskDialog(task?: Task, taskIndex?: number) {
+    const dialogConfig = new MatDialogConfig();
+    dialogConfig.width = '400px';
+
+    if (task) {
+      dialogConfig.data = {
+        task: { ...task },
+        isNew: false,
+        boardId: this.board.id,
+        taskIndex: taskIndex,
+      };
+      console.log('BoardComponent-UpdateTask-DataSent', dialogConfig); // CONSOLE LOG
+    } else {
+      dialogConfig.data = { task: { label: 'purple' }, isNew: true };
+      console.log('BoardComponent-NewTask-DataSent', dialogConfig); // CONSOLE LOG
+    }
+
+    const dialogRef = this.dialog.open(TaskDialogComponent, dialogConfig);
+
+    dialogRef.afterClosed().subscribe((taskData) => {
+      console.log('BoardComponent-DataReceivedBack', taskData); // CONSOLE LOG
+      if (taskData) {
+        if (taskData.isNew && this.board.id) {
+          this.boardService.updateTasks(this.board.id, [
+            ...this.board.tasks,
+            taskData.task,
+          ]);
+        } else if (this.board.id) {
+          const update = this.board.tasks;
+          update.splice(taskData.taskIndex, 1, taskData.task);
+          this.boardService.updateTasks(this.board.id, this.board.tasks);
+        }
+      }
+    });
   }
 }
