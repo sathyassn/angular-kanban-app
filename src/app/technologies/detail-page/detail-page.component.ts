729a4b9ce6f092ed699827ed526506d62cf26ea3
Sathya Narayanan S <sathyassn@gmail.com>
2020-12-11 13:18:02 -0500

    Angular Kanban app - technologies/ detail-page updated

--- src/app/technologies/detail-page/detail-page.component.ts
+++ src/app/technologies/detail-page/detail-page.component.ts
@@ -1,15 +1,47 @@
 import { Component, OnInit } from '@angular/core';
+import { AngularFirestore } from '@angular/fire/firestore';
+import { ActivatedRoute } from '@angular/router';
+import { Observable } from 'rxjs';
+import { map, tap } from 'rxjs/operators';
+import { SeoService } from '../../services/seo.service';
+import { Technology } from '../technology.model';
 
 @Component({
   selector: 'app-detail-page',
   templateUrl: './detail-page.component.html',
-  styleUrls: ['./detail-page.component.scss']
+  styleUrls: ['./detail-page.component.scss'],
 })
 export class DetailPageComponent implements OnInit {
+  techId: string = '';
+  technology!: Observable<Technology>;
 
-  constructor() { }
+  constructor(
+    private route: ActivatedRoute,
+    private db: AngularFirestore,
+    private seo: SeoService
+  ) {}
 
   ngOnInit(): void {
-  }
+    this.techId = <string>this.route.snapshot.paramMap.get('techId');
 
+    this.technology = this.db
+      .doc<Technology>(`technologies/${this.techId}`)
+      .snapshotChanges()
+      .pipe(
+        map((snap) => {
+          console.log('Tech Snap', snap);
+          return <Technology>{
+            id: snap.payload.id,
+            ...snap.payload.data(),
+          };
+        }),
+        tap((tech) => {
+          this.seo.generateTags({
+            title: tech.name,
+            description: tech.description,
+            image: tech.image,
+          });
+        })
+      );
+  }
 }
