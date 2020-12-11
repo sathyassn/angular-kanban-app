0dd9d92822f565305d61d3541ba6d8b84e72cb32
Sathya Narayanan S <sathyassn@gmail.com>
2020-12-11 09:46:53 -0500

    Angular Kanban app - kanban & app-routing mod updated)

--- src/app/kanban/kanban.module.ts
+++ src/app/kanban/kanban.module.ts
@@ -1,14 +1,23 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
+import { ReactiveFormsModule } from '@angular/forms';
+import { DragDropModule } from '@angular/cdk/drag-drop';
+import { MatButtonToggleModule } from '@angular/material/button-toggle';
+import { MatDialogModule } from '@angular/material/dialog';
 
 import { KanbanRoutingModule } from './kanban-routing.module';
-
+import { SharedModule } from '../shared/shared.module';
 
 @NgModule({
   declarations: [],
   imports: [
     CommonModule,
-    KanbanRoutingModule
-  ]
+    KanbanRoutingModule,
+    SharedModule,
+    ReactiveFormsModule,
+    DragDropModule,
+    MatButtonToggleModule,
+    MatDialogModule,
+  ],
 })
-export class KanbanModule { }
+export class KanbanModule {}
