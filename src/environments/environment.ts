ec6fea69e39bddd5ee3085751004298e5b6f5856
Sathya Narayanan S <sathyassn@gmail.com>
2020-12-10 22:28:16 -0500

    Angular Kanban app - Dev & Prod environment updated with Firebase config)

--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -3,7 +3,16 @@
 // The list of file replacements can be found in `angular.json`.
 
 export const environment = {
-  production: false
+  production: false,
+  firebaseConfig: {
+    apiKey: 'AIzaSyAA7PkSBbLmD34T6fhgrZ7BJSy8wMA0x9E',
+    authDomain: 'angularfire-my-app.firebaseapp.com',
+    projectId: 'angularfire-my-app',
+    storageBucket: 'angularfire-my-app.appspot.com',
+    messagingSenderId: '174818100671',
+    appId: '1:174818100671:web:b23f4cd4ebc71fc63d0d0a',
+    measurementId: 'G-Z9YE0KJ0GP',
+  },
 };
 
 /*
