c44ab66155c041ae50dce7bd1a66f6cdc5f240d2
Sathya Narayanan S <sathyassn@gmail.com>
2020-12-10 23:14:43 -0500

    Angular Kanban app - auth guard & snack service added, app-routing updated)

---
+++ src/app/user/auth.guard.ts
@@ -0,0 +1,27 @@
+import { Injectable } from '@angular/core';
+import { AngularFireAuth } from '@angular/fire/auth';
+import {
+  CanActivate,
+  ActivatedRouteSnapshot,
+  RouterStateSnapshot,
+} from '@angular/router';
+import { SnackService } from '../services/snack.service';
+
+@Injectable({
+  providedIn: 'root',
+})
+export class AuthGuard implements CanActivate {
+  constructor(private afAuth: AngularFireAuth, private snack: SnackService) {}
+
+  async canActivate(
+    route: ActivatedRouteSnapshot,
+    state: RouterStateSnapshot
+  ): Promise<boolean> {
+    const user = await this.afAuth.currentUser;
+    const isLoggedIn = !!user;
+    if (!isLoggedIn) {
+      this.snack.authError();
+    }
+    return isLoggedIn;
+  }
+}
