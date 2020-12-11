19d0278e4392262cfedd695a049e3c63588b7e2a
Sathya Narayanan S <sathyassn@gmail.com>
2020-12-11 09:54:14 -0500

    Angular Kanban app - kanban board service updated

--- src/app/kanban/board.service.ts
+++ src/app/kanban/board.service.ts
@@ -1,9 +1,95 @@
 import { Injectable } from '@angular/core';
+import { AngularFireAuth } from '@angular/fire/auth';
+import { AngularFirestore } from '@angular/fire/firestore';
+// import * as firebase from 'firebase/app';
+import firebase from 'firebase';
+import { switchMap, map } from 'rxjs/operators';
+
+import { Board, Task } from './board.model';
 
 @Injectable({
-  providedIn: 'root'
+  providedIn: 'root',
 })
 export class BoardService {
+  constructor(private afAuth: AngularFireAuth, private db: AngularFirestore) {}
 
-  constructor() { }
+  /**
+   * Create new board for the current user
+   */
+  async createBoard(title: string, priority: number) {
+    const user = await this.afAuth.currentUser;
+    return this.db.collection('boards').add({
+      title: title,
+      priority: priority,
+      uid: user?.uid,
+      tasks: [{ description: 'Hello!', label: 'yellow' }],
+    });
+  }
+
+  /**
+   * Delete Board
+   */
+  deleteBoard(boardId: string) {
+    return this.db.collection('boards').doc(boardId).delete();
+  }
+
+  /**
+   * Update the tasks on board
+   */
+  updateTasks(boardId: string, tasks: Task[]) {
+    return this.db.collection('boards').doc(boardId).update({ tasks });
+  }
+
+  /**
+   * Remove Task
+   */
+  removeTask(boardId: string, task: Task) {
+    return this.db
+      .collection('boards')
+      .doc(boardId)
+      .update({
+        tasks: firebase.firestore.FieldValue.arrayRemove(task),
+      });
+  }
+
+  /**
+   * Get User Boards
+   */
+  getUserBoards() {
+    return this.afAuth.authState.pipe(
+      switchMap((user) => {
+        if (user) {
+          return this.db
+            .collection<Board>('boards', (ref) =>
+              ref.where('uid', '==', user.uid).orderBy('priority')
+            )
+            .snapshotChanges()
+            .pipe(
+              map((snaps) =>
+                snaps.map((snap) => {
+                  return <Board>{
+                    id: snap.payload.doc.id,
+                    ...snap.payload.doc.data(),
+                  };
+                })
+              )
+            );
+        } else {
+          return [];
+        }
+      })
+    );
+  }
+
+  /**
+   * Batch write to change the priority of each board for sorting
+   */
+  sortBoards(boards: Board[]) {
+    const batch = this.db.firestore.batch();
+    const refs: any[] = boards.map((b) =>
+      this.db.collection('boards').doc(b.id)
+    );
+    refs.forEach((ref, idx) => batch.update(ref, { priority: idx }));
+    batch.commit();
+  }
 }
