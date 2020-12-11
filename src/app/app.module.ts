c1c8d24722f5269ff0e58bbcfb99d01d09111aa9
Sathya Narayanan S <sathyassn@gmail.com>
2020-12-10 22:30:10 -0500

    Angular Kanban app - AngularFireModule imported to App Module & firebase config initialize added)

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,11 +1,13 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import { AngularFireModule } from '@angular/fire';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { SharedModule } from './shared/shared.module';
 import { HomePageComponent } from './home-page/home-page.component';
+import { environment } from '../environments/environment';
 
 @NgModule({
   declarations: [AppComponent, HomePageComponent],
@@ -14,6 +16,7 @@ import { HomePageComponent } from './home-page/home-page.component';
     AppRoutingModule,
     BrowserAnimationsModule,
     SharedModule,
+    AngularFireModule.initializeApp(environment.firebaseConfig),
   ],
   providers: [],
   bootstrap: [AppComponent],
