92bb4c28f8a895dcfaded290cd2de1ee5cf945b4
Sathya Narayanan S <sathyassn@gmail.com>
2020-12-10 23:20:37 -0500

    Angular Kanban app - shell comp updated)

--- src/app/shared/shell/shell.component.ts
+++ src/app/shared/shell/shell.component.ts
@@ -2,6 +2,9 @@ import { Component, ViewChild } from '@angular/core';
 import { BreakpointObserver, Breakpoints } from '@angular/cdk/layout';
 import { Observable } from 'rxjs';
 import { map, shareReplay } from 'rxjs/operators';
+import { AngularFireAuth } from '@angular/fire/auth';
+import { Router } from '@angular/router';
+import { MatSidenav } from '@angular/material/sidenav';
 
 @Component({
   selector: 'app-shell',
@@ -16,5 +19,18 @@ export class ShellComponent {
       shareReplay()
     );
 
-  constructor(private breakpointObserver: BreakpointObserver) {}
+  @ViewChild('drawer')
+  sideNav?: MatSidenav;
+
+  constructor(
+    private breakpointObserver: BreakpointObserver,
+    public afAuth: AngularFireAuth,
+    private router: Router
+  ) {}
+
+  signout() {
+    this.afAuth.signOut();
+    this.router.navigateByUrl('/login');
+    this.sideNav?.close();
+  }
 }
