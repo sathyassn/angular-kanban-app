1b0e4809abe4c1da26d801a0fd03377ea851d6e2
Sathya Narayanan S <sathyassn@gmail.com>
2020-12-11 10:23:15 -0500

    Angular Kanban app - kanban board-dialog created

--- src/app/kanban/kanban.module.ts
+++ src/app/kanban/kanban.module.ts
@@ -9,9 +9,10 @@ import { KanbanRoutingModule } from './kanban-routing.module';
 import { SharedModule } from '../shared/shared.module';
 import { BoardListComponent } from './board-list/board-list.component';
 import { BoardComponent } from './board/board.component';
+import { BoardDialogComponent } from './dialogs/board-dialog/board-dialog.component';
 
 @NgModule({
-  declarations: [BoardListComponent, BoardComponent],
+  declarations: [BoardListComponent, BoardComponent, BoardDialogComponent],
   imports: [
     CommonModule,
     KanbanRoutingModule,
