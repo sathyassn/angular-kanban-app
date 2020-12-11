e9bd6afe16bd36280a2cea4f58fc4cc43ed5e908
Sathya Narayanan S <sathyassn@gmail.com>
2020-12-11 10:17:14 -0500

    Angular Kanban app - kanban board comp updated

--- src/app/kanban/board/board.component.ts
+++ src/app/kanban/board/board.component.ts
@@ -1,15 +1,28 @@
-import { Component, OnInit } from '@angular/core';
+import { CdkDragDrop, moveItemInArray } from '@angular/cdk/drag-drop';
+import { Component, Input } from '@angular/core';
+import { MatDialog } from '@angular/material/dialog';
+
+import { Board } from '../board.model';
+import { BoardService } from '../board.service';
 
 @Component({
   selector: 'app-board',
   templateUrl: './board.component.html',
-  styleUrls: ['./board.component.scss']
+  styleUrls: ['./board.component.scss'],
 })
-export class BoardComponent implements OnInit {
+export class BoardComponent {
+  @Input() board!: Board; // Non-Null Assertion Operator used!
 
-  constructor() { }
+  constructor(private boardService: BoardService, private dialog: MatDialog) {}
 
-  ngOnInit(): void {
+  async taskDrop(event: CdkDragDrop<string[]>) {
+    if (this.board?.id) {
+      moveItemInArray(
+        this.board.tasks,
+        event.previousIndex,
+        event.currentIndex
+      );
+      this.boardService.updateTasks(this.board.id, this.board.tasks);
+    }
   }
-
 }
