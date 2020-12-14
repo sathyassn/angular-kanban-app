f651d88b412d871c8e82a98c1101cdd03908319c
Sathya Narayanan S <sathyassn@gmail.com>
2020-12-14 17:14:50 -0500

    Angular Kanban app - board service  updated

--- src/app/kanban/board.service.ts
+++ src/app/kanban/board.service.ts
@@ -1,9 +1,9 @@
 import { Injectable } from '@angular/core';
 import { AngularFireAuth } from '@angular/fire/auth';
 import { AngularFirestore } from '@angular/fire/firestore';
-// import * as firebase from 'firebase/app';
 import firebase from 'firebase';
 import { switchMap, map } from 'rxjs/operators';
+import { v4 as uuidv4 } from 'uuid';
 
 import { Board, Task } from './board.model';
 
@@ -18,12 +18,17 @@ export class BoardService {
    */
   async createBoard(title: string, priority: number) {
     const user = await this.afAuth.currentUser;
-    return this.db.collection('boards').add({
-      title: title,
-      priority: priority,
-      uid: user?.uid,
-      tasks: [{ description: 'Hello!', label: 'yellow' }],
-    });
+    const id = uuidv4();
+    return this.db
+      .collection('boards')
+      .doc(id)
+      .set({
+        boardId: id,
+        title: title,
+        priority: priority,
+        uid: user?.uid,
+        tasks: [{ description: 'Hello!', label: 'yellow' }],
+      });
   }
 
   /**
@@ -49,6 +54,18 @@ export class BoardService {
       .doc(boardId)
       .update({
         tasks: firebase.firestore.FieldValue.arrayRemove(task),
+      });
+  }
+
+  /**
+   * Add Task
+   */
+  addTask(boardId: string, task: Task) {
+    return this.db
+      .collection('boards')
+      .doc(boardId)
+      .update({
+        tasks: firebase.firestore.FieldValue.arrayUnion(task),
       });
   }
 
@@ -86,10 +103,31 @@ export class BoardService {
    */
   sortBoards(boards: Board[]) {
     const batch = this.db.firestore.batch();
-    const refs: any[] = boards.map((b) =>
-      this.db.collection('boards').doc(b.id)
+    const refs: any[] = boards.map(
+      (b) => this.db.collection('boards').doc(b.boardId).ref
     );
     refs.forEach((ref, idx) => batch.update(ref, { priority: idx }));
     batch.commit();
+  }
+
+  /**
+   * Obtain length of task array in a board
+   */
+  taskLength(boardId: string) {
+    return this.db
+      .collection<Board>('boards')
+      .doc(boardId)
+      .snapshotChanges()
+      .pipe(
+        map((snap) => {
+          const boardData = snap.payload.data();
+          const taskLength = boardData?.tasks.length;
+          if (typeof taskLength === 'number' && taskLength > 0) {
+            return taskLength;
+          } else {
+            return 0;
+          }
+        })
+      );
   }
 }
