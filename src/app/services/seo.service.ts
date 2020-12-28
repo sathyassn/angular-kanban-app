5ba5b83636cdfe3cb8ac9cbde2e3f6739fb9afe7
Sathya Narayanan S <sathyassn@gmail.com>
2020-12-28 06:33:12 -0500

    Angular Kanban app - seo service updated

--- src/app/services/seo.service.ts
+++ src/app/services/seo.service.ts
@@ -19,13 +19,16 @@ export class SeoService {
       { name: 'description', content: description },
       // Open Graph
       { name: 'og:url', content: ` ${this.router.url}` },
-      { name: 'og:title', content: title },
+      { name: 'og:title', content: `Kanban | ${title}` },
       { name: 'og:description', content: description },
       { name: 'og:image', content: image },
 
       // Twitter Card
+      { name: 'twitter:title', content: `Kanban | ${title}` },
       { name: 'twitter:card', content: 'summary' },
       { name: 'twitter:site', content: '@sathyassn' },
+      { name: 'twitter:description', content: description },
+      { name: 'twitter:image', content: image },
     ]);
   }
 }
